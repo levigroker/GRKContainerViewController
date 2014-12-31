d93cec620fe42afd3bc931e6c306dc74cdd5299f
Levi Brown <levigroker@gmail.com>
2014-12-31 15:38:53 -0700

    Fixing an issue where re-setting the same view controller object would remove the view controller.

--- GRKContainerViewController/GRKContainerViewController.m
+++ GRKContainerViewController/GRKContainerViewController.m
@@ -62,54 +62,65 @@ NSTimeInterval const kDefaultAnimationDuration = 0.5f;
 
 - (void)setViewController:(UIViewController *)viewController animated:(BOOL)animated completion:(void(^)(UIViewController *viewController))completion
 {
-    //Ensure the view is loaded
-    [self view];
-    
-    //We will use a simple cross fade animation, as indicated
-    NSTimeInterval duration = animated ? self.transitionAnimationDuration : 0.0f;
-    
-    // We're fading out views, so make sure they're not invisible when we start.
-    self.viewController.view.alpha = 1.0f;
-    viewController.view.alpha = 1.0f;
-    
-    //Add the new view controller to the view and view controller hierarchies
-    if (viewController)
+    if ([self.viewController isEqual:viewController])
     {
-        [self addChildViewController:viewController];
-        
-        // Add the new view below the current view, and then fade out the current view.
-        [self.view addSubview:viewController.view];
-        [self.view insertSubview:viewController.view belowSubview:self.viewController.view];
-        [viewController didMoveToParentViewController:self];
-
-        //Setup constraints to keep the new view pinned to our size.
-        UIView *containedView = viewController.view;
-        containedView.translatesAutoresizingMaskIntoConstraints = NO;
-        [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|[containedView]|" options:0 metrics:nil views:NSDictionaryOfVariableBindings(containedView)]];
-        [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|[containedView]|" options:0 metrics:nil views:NSDictionaryOfVariableBindings(containedView)]];
-    }
-    
-    // Hold onto the old controller, and set the new one.
-    UIViewController *oldViewController = self.viewController;
-    _viewController = viewController;
-    
-    [UIView animateWithDuration:duration animations:^{
-        // Animate away the old controller view.
-        oldViewController.view.alpha = 0.0f;
-    } completion:^(BOOL finished) {
-        // Quick transitions can leave us trying to remove the view that has just been added. Only remove it if its not the same controller.
-        if (_viewController != oldViewController) {
-            [oldViewController willMoveToParentViewController:nil];
-            [oldViewController.view removeFromSuperview];
-            [oldViewController removeFromParentViewController];
-        }
-        
         if (completion)
         {
-            //Call the completion block with the new view controller
+            //Call the completion block with the view controller
             completion(viewController);
         }
-    }];
+    }
+    else
+    {
+        //Ensure the view is loaded
+        [self view];
+        
+        //We will use a simple cross fade animation, as indicated
+        NSTimeInterval duration = animated ? self.transitionAnimationDuration : 0.0f;
+        
+        // We're fading out views, so make sure they're not invisible when we start.
+        self.viewController.view.alpha = 1.0f;
+        viewController.view.alpha = 1.0f;
+        
+        //Add the new view controller to the view and view controller hierarchies
+        if (viewController)
+        {
+            [self addChildViewController:viewController];
+            
+            // Add the new view below the current view, and then fade out the current view.
+            [self.view addSubview:viewController.view];
+            [self.view insertSubview:viewController.view belowSubview:self.viewController.view];
+            [viewController didMoveToParentViewController:self];
+            
+            //Setup constraints to keep the new view pinned to our size.
+            UIView *containedView = viewController.view;
+            containedView.translatesAutoresizingMaskIntoConstraints = NO;
+            [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|[containedView]|" options:0 metrics:nil views:NSDictionaryOfVariableBindings(containedView)]];
+            [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|[containedView]|" options:0 metrics:nil views:NSDictionaryOfVariableBindings(containedView)]];
+        }
+        
+        // Hold onto the old controller, and set the new one.
+        UIViewController *oldViewController = self.viewController;
+        _viewController = viewController;
+        
+        [UIView animateWithDuration:duration animations:^{
+            // Animate away the old controller view.
+            oldViewController.view.alpha = 0.0f;
+        } completion:^(BOOL finished) {
+            // Quick transitions can leave us trying to remove the view that has just been added. Only remove it if its not the same controller.
+            if (_viewController != oldViewController) {
+                [oldViewController willMoveToParentViewController:nil];
+                [oldViewController.view removeFromSuperview];
+                [oldViewController removeFromParentViewController];
+            }
+            
+            if (completion)
+            {
+                //Call the completion block with the new view controller
+                completion(viewController);
+            }
+        }];
+    }
 }
 
 #pragma mark - Accessors
