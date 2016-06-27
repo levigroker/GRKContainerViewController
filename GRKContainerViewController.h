2265dd080c6597115032be8194b0bb0d37d01397
Slipp Douglas Thompson <git@capnslipp.com>
2016-06-27 18:39:40 -0500

    Created Cocoa Touch Framework proj for Carthage-compat
    * Created a new `GRKContainerViewController.xcodeproj` from the Cocoa Touch Framework template.
    * Added the existing `GRKContainerViewController/GRKContainerViewController.h`/`.m` files to the project.
    * Set up the template-generated header with the current version number (1.2) and an import for `<GRKContainerViewController/GRKContainerViewController.h>`.
    * Changed `Info.plist`'s `CFBundleShortVersionString` to also (in addition to `CFBundleVersion`) use the build setting `CURRENT_PROJECT_VERSION`'s value.
    * Changed the `CURRENT_PROJECT_VERSION` build setting to the current version (1.2).
    * Shared the “GRKContainerViewController” scheme (required by Carthage).
    * Set iOS Deployment Target to 8.0, the oldest iOS version that supports frameworks.

---
+++ GRKContainerViewController.h
@@ -0,0 +1,29 @@
+//
+//  GRKContainerViewController.h
+//
+//  Created by Cap'n Slipp on 6/27/16.
+//  Copyright (c) 2013, 2014 Levi Brown <mailto:levigroker@gmail.com>
+//  This work is licensed under the Creative Commons Attribution 3.0
+//  Unported License. To view a copy of this license, visit
+//  http://creativecommons.org/licenses/by/3.0/ or send a letter to Creative
+//  Commons, 444 Castro Street, Suite 900, Mountain View, California, 94041,
+//  USA.
+//
+//  The above attribution and the included license must accompany any version
+//  of the source code. Visible attribution in any binary distributable
+//  including this work (or derivatives) is not required, but would be
+//  appreciated.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GRKContainerViewController.
+FOUNDATION_EXPORT double GRKContainerViewControllerVersionNumber = 1.2;
+
+//! Project version string for GRKContainerViewController.
+FOUNDATION_EXPORT const unsigned char GRKContainerViewControllerVersionString[] = "1.2";
+
+
+#import <GRKContainerViewController/GRKContainerViewController.h>
+
+
